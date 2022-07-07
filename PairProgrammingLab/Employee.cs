26af23fea09c0d5cafd602efc8d1e0871ae21d1c
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-07 14:31:42 -0400

    Created Employee class

---
+++ PairProgrammingLab/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgrammingLab
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Position { get; set; }
+
+        public Employee(string name, double salary, string position)
+        {
+            Name = name;
+            Salary = salary;
+            Position = position;
+        }
+    }
+}
