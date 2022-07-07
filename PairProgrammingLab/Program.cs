cc2eb1903abcf129d4c7c2f523b71d21e81a0977
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-07-07 14:42:13 -0400

    Added employees

--- PairProgrammingLab/Program.cs
+++ PairProgrammingLab/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using PairProgrammingLab;
+
+Employee a = new Employee("Dan", 15, "Starving Artist");
+Employee b = new Employee("Asia", 150000, "Master Chef");
+Employee c = new Employee("Ali", 420, "Jazz Cabbage Farmer");
+
